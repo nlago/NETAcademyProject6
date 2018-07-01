94df9252e873bdca56567fa685f87707a7b5526d
Angelos Lagopoulos <nglago24@gmail.com>
2018-07-01 17:15:17 +0300

    code cleanup, remove unused files

--- CFProject_T6/Controllers/BackersProjectsController.cs
+++ CFProject_T6/Controllers/BackersProjectsController.cs
@@ -20,41 +20,19 @@ namespace CFProject_T6.Controllers
         public BackersProjectsController(ProjectContext context)
         {
             _context = context;
-        }
-
-        // GET: BackersProjects
-        public async Task<IActionResult> Index()
-        {
-            var projectContext = _context.BackersProjects.Include(b => b.Package).Include(b => b.Project).Include(b => b.User);
-            return View(await projectContext.ToListAsync());
-        }
-
-        // GET: BackersProjects/Details/5
-        public async Task<IActionResult> Details(long? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var backersProjects = await _context.BackersProjects
-                .Include(b => b.Package)
-                .Include(b => b.Project)
-                .Include(b => b.User)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (backersProjects == null)
-            {
-                return NotFound();
-            }
-
-            return View(backersProjects);
-        }
+        }              
 
         [Authorize]
-        // GET: BackersProjects/Create
-        [HttpGet("project/{project_id:long}/packages/details/{package_id}/purchase", Name = "purchase")]
+        [HttpGet("project/{project_id:long}/packages/{package_id}/purchase", Name = "purchase")]
         public async Task<IActionResult> Create(long package_id, long project_id)
         {
+            var selected_project_exists = _context.Projects.Any(p => p.Id == project_id);
+            var selected_package_exists = _context.Packages.Any(p => p.Id == package_id);
+            if (!selected_project_exists || !selected_package_exists)
+            {
+                return RedirectToAction("Index", "Project");
+            }
+
             //initialize required vars
             long userid = GetUserID();
             var live = false;
@@ -109,15 +87,10 @@ namespace CFProject_T6.Controllers
 
             return View(invalid_purchase);
 
-            //ViewData["PackageId"] = new SelectList(_context.Packages, "Id", "Reward");
-            //ViewData["ProjectId"] = new SelectList(_context.Projects, "Id", "Descr");
-            //ViewData["UserId"] = new SelectList(_context.Users, "Id", "Email");
-
         }
 
-        // POST: BackersProjects/Create
         [Authorize]
-        [HttpPost("project/{project_id:long}/packages/details/{package_id}/purchase", Name = "purchase")]
+        [HttpPost("project/{project_id:long}/packages/{package_id}/purchase", Name = "purchase")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create (BackersProjects backersProjects, long project_id, long package_id)
         {
@@ -130,7 +103,7 @@ namespace CFProject_T6.Controllers
 
             if (selected_project_startdate > DateTime.Now)
             {
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction("Index", "Packages");
             }
             //<---------- protection code ends here ---------->
 
@@ -170,108 +143,19 @@ namespace CFProject_T6.Controllers
                 CheckGoal.Getmails(Project, UserList);
                 
 
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction("Index", "Packages");
             }
 
             ViewData["PackageId"] = new SelectList(_context.Packages, "Id", "Reward", backersProjects.PackageId);
             ViewData["ProjectId"] = new SelectList(_context.Projects, "Id", "Descr", backersProjects.ProjectId);
             ViewData["UserId"] = new SelectList(_context.Users, "Id", "Email", backersProjects.UserId);
             return View(backersProjects);
-        }
+        }        
 
-        // GET: BackersProjects/Edit/5
-        public async Task<IActionResult> Edit(long? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var backersProjects = await _context.BackersProjects.FindAsync(id);
-            if (backersProjects == null)
-            {
-                return NotFound();
-            }
-            ViewData["PackageId"] = new SelectList(_context.Packages, "Id", "Reward", backersProjects.PackageId);
-            ViewData["ProjectId"] = new SelectList(_context.Projects, "Id", "Descr", backersProjects.ProjectId);
-            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Email", backersProjects.UserId);
-            return View(backersProjects);
-        }
-
-        // POST: BackersProjects/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(long id, [Bind("Id,UserId,ProjectId,PackageId")] BackersProjects backersProjects)
-        {
-            if (id != backersProjects.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(backersProjects);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!BackersProjectsExists(backersProjects.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["PackageId"] = new SelectList(_context.Packages, "Id", "Reward", backersProjects.PackageId);
-            ViewData["ProjectId"] = new SelectList(_context.Projects, "Id", "Descr", backersProjects.ProjectId);
-            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Email", backersProjects.UserId);
-            return View(backersProjects);
-        }
-
-        // GET: BackersProjects/Delete/5
-        public async Task<IActionResult> Delete(long? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var backersProjects = await _context.BackersProjects
-                .Include(b => b.Package)
-                .Include(b => b.Project)
-                .Include(b => b.User)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (backersProjects == null)
-            {
-                return NotFound();
-            }
-
-            return View(backersProjects);
-        }
-
-        // POST: BackersProjects/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(long id)
-        {
-            var backersProjects = await _context.BackersProjects.FindAsync(id);
-            _context.BackersProjects.Remove(backersProjects);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool BackersProjectsExists(long id)
-        {
-            return _context.BackersProjects.Any(e => e.Id == id);
-        }
+        //private bool BackersProjectsExists(long id)
+        //{
+        //    return _context.BackersProjects.Any(e => e.Id == id);
+        //}
 
         private long GetUserID()
         {
